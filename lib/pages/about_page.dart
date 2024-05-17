b12dcdd95115558843dbaaf4367ee777145ced9a
Salehshalab87 <salehshalab2@gmail.com>
2024-05-17 20:51:32 +0300

    ok

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -13,6 +13,8 @@ class AboutPage extends StatelessWidget {
           style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -22,69 +24,73 @@ class AboutPage extends StatelessWidget {
             const SizedBox(height: 30),
             CircleAvatar(
               backgroundColor: Colors.grey[300],
-              radius: 70,
+              radius: 80,
               backgroundImage: const AssetImage('images/slogo.png'),
             ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 40),
             const Text(
               'Eventro',
               style: TextStyle(
-                fontSize: 30,
+                fontSize: 36,
                 fontWeight: FontWeight.bold,
+                color: Colors.black87,
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 15),
             const Text(
               'Version 1.0.0',
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 20,
+                color: Colors.black54,
               ),
             ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 40),
             const Text(
               'Eventro is an app that helps you discover and manage events in your area. Whether you\'re looking for concerts, festivals, conferences, or sports events, Eventro has you covered.',
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 16,
+                fontSize: 18,
+                color: Colors.black87,
               ),
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 60),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/developer.png'),
+                CircleAvatar(
+                  backgroundColor: Colors.grey[300],
+                  radius: 60,
+                  backgroundImage: const AssetImage('images/developer.png'),
                 ),
-                const SizedBox(width: 10),
+                const SizedBox(width: 20),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
                       'Saleh Shalab',
                       style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
+                        color: Colors.black87,
                       ),
                     ),
                     const SizedBox(height: 10),
                     const Text(
                       'Software Developer',
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 18,
+                        color: Colors.black54,
                       ),
                     ),
-                    const SizedBox(height: 10),
-                    OutlinedButton(
-                      style: const ButtonStyle(
-                          side: WidgetStatePropertyAll<BorderSide>(
-                            BorderSide(
-                                color: Color(
-                                    0xffEC6408)), // Change the color to your desired color
-                          ),
-                          foregroundColor: WidgetStatePropertyAll(Colors.black),
-                          backgroundColor:
-                              WidgetStatePropertyAll(Color(0xffEC6408))),
+                    const SizedBox(height: 12),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: const Color(0xffEC6408),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
                       onPressed: () {
                         final Uri emailLaunchUri = Uri(
                           scheme: 'mailto',
@@ -92,7 +98,14 @@ class AboutPage extends StatelessWidget {
                         );
                         launchUrl(emailLaunchUri);
                       },
-                      child: const Text('Contact Me ->'),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                        child: Text(
+                          'Contact Me',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
                     ),
                   ],
                 ),
